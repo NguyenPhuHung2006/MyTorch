6fa9150f8e332f0af780f16095bdd781acf843fb
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2026-07-08 11:41:44 +0700

    fix the matmul bug when one of them is a vector

--- mytorch/ops/linalg.py
+++ mytorch/ops/linalg.py
@@ -14,6 +14,8 @@ class MatMul(Function):
         ctx.saved_data["y_shape"] = (
             y.data.shape if isinstance(y, Tensor) else None
         )
+        ctx.saved_data["x_ndim"] = x.data.ndim
+        ctx.saved_data["y_ndim"] = y.data.ndim if isinstance(y, Tensor) else np.asarray(y).ndim
         return x.data @ (y.data if isinstance(y, Tensor) else y)
     
     @staticmethod
@@ -21,11 +23,32 @@ class MatMul(Function):
         x_data = ctx.saved_data["x_data"]
         y_data = ctx.saved_data["y_data"]
         y_is_tensor = ctx.saved_data["y_is_tensor"]
+        x_ndim = ctx.saved_data["x_ndim"]
+        y_ndim = ctx.saved_data["y_ndim"]
+        
+        if x_ndim == 1:
+            x_data = np.expand_dims(x_data, -2)
+        if y_ndim == 1:
+            y_data = np.expand_dims(y_data, -1)
+            
+        if grad_output.ndim == 0:
+            grad_output = grad_output.reshape(1, 1)
+        elif x_ndim == 1:
+            grad_output = np.expand_dims(grad_output, -2)
+        elif y_ndim == 1:
+            grad_output = np.expand_dims(grad_output, -1)
         
         grad_x = grad_output @ matrix_transpose(y_data)
         grad_y = matrix_transpose(x_data) @ grad_output if y_is_tensor else None
+        
+        if x_ndim == 1:
+            grad_x = np.squeeze(grad_x, axis=-2)
+        if y_ndim == 1:
+            grad_y = np.squeeze(grad_y, axis=-1)
+            
         grad_x = unbroadcast(grad_x, ctx.saved_data["x_shape"])
-        grad_y = unbroadcast(grad_y, ctx.saved_data["y_shape"])
+        if y_is_tensor:
+            grad_y = unbroadcast(grad_y, ctx.saved_data["y_shape"])
         
         return grad_x, grad_y
         
