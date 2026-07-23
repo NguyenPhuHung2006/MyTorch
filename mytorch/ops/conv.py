306582b3a5d82f9f1918b4d0de8ac00b94128701
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2026-07-23 16:24:00 +0700

    quit implementing conv for now

--- mytorch/ops/conv.py
+++ mytorch/ops/conv.py
@@ -12,38 +12,7 @@ class ConvNd(Function):
                 padding: tuple,
                 dilation: tuple
             ):
-        
-        ctx.save_for_backward(x)
-        
-        batch_size = x.shape[0]
-        out_channels = weight.shape[0]
-        input_sizes = x.shape[2:]
-        kernel_sizes = weight.shape[2:]
-        
-        output_sizes = tuple(
-            (input_size + 2 * pad - dil * (kernel_size - 1) - 1) // stride + 1 
-            for input_size, pad, kernel_size, dil, stride 
-            in zip(input_sizes, padding, kernel_sizes, dilation, stride)
-        )
-        
-        output_shape = (batch_size, out_channels,) + output_sizes
-        
-        output = np.zeros(output_shape)
-        
-        kernel_indices = np.ndindex(kernel_sizes)
-        in_channels = weight.shape[1]
-        
-        for output_index in np.ndindex(output_shape):
-            batch_index, out_channel_index, output_size_index = output_index
-            
-            for kernel_index in kernel_indices:
-                for in_channel_index in range(in_channels):
-                    input_index = tuple(
-                        i * stride + k * dil 
-                        for i, stride, k, dil 
-                        in zip(output_index, stride, kernel_index, dilation)
-                    )
-                    
+        ...          
         
 
     @staticmethod
